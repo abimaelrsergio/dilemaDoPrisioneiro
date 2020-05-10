4e9ac6935a9e7de4658d90b2cf0d848f2dba7c65
Abimael Rodrigues Sergio <abimaelr.sergio@gmail.com>
2020-05-09 23:35:28 -0300

    Atualizacao

--- src/main/java/br/com/abgi/model/Individuo.java
+++ src/main/java/br/com/abgi/model/Individuo.java
@@ -2,7 +2,7 @@ package br.com.abgi.model;
 
 import java.util.Random;
 
-import br.com.abgi.enums.EnumTamanhos;
+import br.com.abgi.enums.EnumConfiguracao;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,15 +14,16 @@ public class Individuo {
 
 	private int[] probabilidadeDeCooperacao; // genoma para a probabilidade de cooperação
 	private float valorDoGenoma; // valor do genoma
+	private int fitness;// Aptidão do indivíduo
 
 	public Individuo() {
-		probabilidadeDeCooperacao = new int[EnumTamanhos.TAMANHO_CROMOSSOMO.getTamanho()];
+		probabilidadeDeCooperacao = new int[EnumConfiguracao.TAMANHO_CROMOSSOMO.getTamanho()];
 		iniciarProbabilidadeDeCooperacao();
 	}
 
 	private void iniciarProbabilidadeDeCooperacao() {
 
-		int tamanhoCromossomo = EnumTamanhos.TAMANHO_CROMOSSOMO.getTamanho();
+		int tamanhoCromossomo = EnumConfiguracao.TAMANHO_CROMOSSOMO.getTamanho();
 
 		for (int posicaoCromossomo = 0; posicaoCromossomo < tamanhoCromossomo; posicaoCromossomo++) {
 
